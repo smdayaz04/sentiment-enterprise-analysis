f10bd572083cc08cf30e6969d3feec2b0dc26e3c
smdayaz04 <smdayaz04@gmail.com>
2026-02-02 17:26:43 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -4,9 +4,9 @@ import config
 def load_data(n=None):
     cols = ['target', 'id', 'date', 'flag', 'user', 'text']
     
-    df = pd.read_csv(config.RAW_FILE, encoding='latin-1', names=cols, nrows=n)
+    # Direct raw link to the 1.6M row dataset
+    url = "https://raw.githubusercontent.com/kaz-Anova/Sentiment140/master/training.1600000.processed.noemoticon.csv"
     
+    df = pd.read_csv(url, encoding='latin-1', names=cols, nrows=n)
     df['label'] = df['target'].replace({0: 'negative', 4: 'positive'})
-    df['timestamp'] = pd.to_datetime(df['date'].str.replace(' PDT ', ' '), errors='coerce')
-    
     return df
